9a48f2c73680c5954fe62ff97968cafab0516144
Corey Ladovsky <coreyskymusic@gmail.com>
2020-05-07 15:29:28 -0400

    update apiURL

--- frontend/src/util/apiURL.js
+++ frontend/src/util/apiURL.js
@@ -1,5 +1,5 @@
 export const apiURL = () => {
   return window.location.hostname === "localhost"
     ? "http://localhost:3001"
-    : "";
+    : "https://auth-practice-for-class.herokuapp.com";
 };
