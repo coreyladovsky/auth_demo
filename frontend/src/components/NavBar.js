380f848dd7c7b873d23708fd9aa5f0b5e018cc22
Corey Ladovsky <coreyskymusic@gmail.com>
2020-05-09 09:42:04 -0400

    nav bar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { NavLink } from 'react-router-dom';
 import "../css/NavBar.css";
+import { AuthContext } from '../providers/AuthProvider';
 import { logout } from '../util/firebaseFunctons';
 
 export default function NavBar(params) {
+    const { user } = useContext(AuthContext);
+    const display = () => {
+        if(user) {
+            return <button onClick={logout}>Log Out</button>;
+        } else {
+            return (
+              <>
+                <NavLink to={"/signup"}>Sign Up</NavLink>
+                <NavLink to={"/login"}>Log In</NavLink>
+              </>
+            );
+        }
+    }
     return(
         <nav>
             <NavLink to={"/"}>Home</NavLink>
             <NavLink to={"/users"}>Show All Users</NavLink>
-            <NavLink to={"/signup"}>Sign Up</NavLink>
-            <NavLink to={"/login"}>Log In</NavLink>
-            <button onClick={logout}>Log Out</button>
+            {display()}
+            
         </nav>
     )
 };
